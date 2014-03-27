b0b276d1e154d1240e8f9c0a56b3c048eb89f6f1
procoderx <rishy.s13@gmail.com>
2014-03-28 02:21:22 +0530

    my custom angular-wizard library used for mifosX community-app

--- dist/angular-wizard.js
+++ dist/angular-wizard.js
@@ -15,12 +15,12 @@ angular.module("step.html", []).run(["$templateCache", function($templateCache) 
 angular.module("wizard.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("wizard.html",
     "<div>\n" +
-    "    <div class=\"steps\" ng-transclude></div>\n" +
     "    <ul class=\"steps-indicator steps-{{steps.length}}\" ng-if=\"!hideIndicators\">\n" +
     "      <li ng-class=\"{default: !step.completed && !step.selected, current: step.selected && !step.completed, done: step.completed && !step.selected, editing: step.selected && step.completed}\" ng-repeat=\"step in steps\">\n" +
     "        <a ng-click=\"goTo(step)\">{{step.title}}</a>\n" +
     "      </li>\n" +
     "    </ul>\n" +
+    "    <div class=\"steps\" ng-transclude></div>\n" +
     "</div>");
 }]);
 
